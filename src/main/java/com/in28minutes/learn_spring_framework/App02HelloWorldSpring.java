80a22136058dc993f57b58f71a15ad3f7d90ee85
Khushi <khushikhoker9@gmail.com>
2025-08-04 00:05:40 +0530

    Primary and Qualifier annotation for issue like multiple matching beans

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -18,7 +18,8 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("person2Methodcall"));
         System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
-//        System.out.println(context.getBean(Address.class));
+        System.out.println(context.getBean("person5Qualifier"));
+        System.out.println(context.getBean(Address.class));
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package com.in28minutes.learn_spring_framework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.naming.Name;
 
@@ -23,6 +25,7 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    @Primary
     public Person person() {
         return new Person("Ram", 27,  new Address("Lane 2 Mandakini Vihar", "xyzCity"));
     }
@@ -37,12 +40,24 @@ public class HelloWorldConfiguration {
         return new Person(name, age, address3);
     }
 
+    @Bean
+    public Person person4Parameters(String name, int age, Address address) {
+        return new Person(name, age, address);
+    }
+
+    @Bean
+    public Person person5Qualifier(String name, int age, @Qualifier("address3Qualifier") Address address) {
+        return new Person(name, age, address);
+    }
+
     @Bean(name = "address2")
+    @Primary
     public Address address() {
         return new Address("Lane 2 Mandakini Vihar", "xyzCity");
     }
 
     @Bean
+    @Qualifier("address3Qualifier")
     public Address address3() {
         return new Address("Labcd dkdhf", "abc city");
     }
