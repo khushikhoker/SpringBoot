ff77d0a55863b1a72a5368141577527c04da4769
Khushi <khushikhoker9@gmail.com>
2025-08-03 20:14:12 +0530

    Reusing existing beans for auto wiring

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -13,7 +13,9 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2Methodcall"));
+        System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("address2"));
-        System.out.println(context.getBean(Address.class));
+//        System.out.println(context.getBean(Address.class));
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,7 +3,9 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {};
+import javax.naming.Name;
+
+record Person(String name, int age, Address address) {};
 
 record Address(String firstLine, String city){};
 
@@ -22,11 +24,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Ram", 27);
+        return new Person("Ram", 27,  new Address("Lane 2 Mandakini Vihar", "xyzCity"));
+    }
+
+    @Bean
+    public Person person2Methodcall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
     }
 
     @Bean(name = "address2")
     public Address address() {
         return new Address("Lane 2 Mandakini Vihar", "xyzCity");
+    }
+
+    @Bean
+    public Address address3() {
+        return new Address("Labcd dkdhf", "abc city");
     }
 }
